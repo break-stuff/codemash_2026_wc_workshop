b64429c53d2c49e95001bc4bce6bc2fc0b591fbe
Burton Smith <burton@breakstuff.io>
2026-01-13 16:14:24 -0500

    adding events and docs

--- src/components/button-2/button-2.stories.ts
+++ src/components/button-2/button-2.stories.ts
@@ -2,6 +2,7 @@ import { StoryObj } from '@storybook/web-components-vite';
 import { getStorybookHelpers } from '@wc-toolkit/storybook-helpers';
 import './index.js';
 import type { MyButton_2 } from './index.js';
+import { html } from 'lit';
 
 const { args, argTypes, events, template } = getStorybookHelpers('my-button-2');
 
@@ -22,5 +23,13 @@ type Story = StoryObj<MyButton_2 & typeof args>;
 export const Default: Story = {
   render: args => template(args),
   args: {
+    'default-slot': 'Click me!'
   }
 };
+
+export const DisabledStory = {
+  render: (args) => html`<my-button ?disabled=${args.disabled}>click me</my-button>`,
+  args: {
+    disabled: true
+  }
+}
--- src/components/button-2/button-2.ts
+++ src/components/button-2/button-2.ts
@@ -9,22 +9,45 @@ import styles from './button-2.styles.js';
  * @since 0.0.0
  * @status experimental
  *
+ * @cssprop [--button-border-radius=4px] - Controls the broder radius of the button
+ * 
+ * @csspart control - adds style hook to button
+ * 
+ * @slot - the button text
+ * @slot icon - the icon before the button
+ * 
+ * @event {CustomEvent<{timeStamp: number}>} high-five - Emitted when the button is clicked
  **/
 export class MyButton_2 extends LitElement {
   static override styles = styles;
 
+  /** Sets the label above the button */
   @property()
   label?: string;
 
+  /** Disables the button when `true` */
   @property({ type: Boolean })
   disabled?: boolean;
+
+  clickHandler() {
+    /** @internal */
+    this.dispatchEvent(
+      new CustomEvent<{ timeStamp: number }>('high-five', {
+        detail: {
+          timeStamp: Date.now(),
+        },
+        bubbles: false
+      }),
+    );
+  }
 
   override render() {
     return html`
       <div>${this.label}</div>
-      <button 
-        part="control" 
+      <button
+        part="control"
         ?disabled=${this.disabled}
+        @click=${this.clickHandler}
       >
         <slot name="icon"></slot>
         <slot></slot>
